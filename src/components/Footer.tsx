f30166c8d4611a875b3748619c9b54325c480b34
Pedro Almeida <pedro.hp.almeida@gmail.com>
2025-04-07 19:43:02 -0300

    refactor: use version from package.json instead of .env

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useLanguage } from '../context/LanguageContext';
 import { translations } from '../translations';
+import packageJson from '../../package.json';
 import './Footer.css';
 
 const Footer: React.FC = () => {
   const { language } = useLanguage();
   const t = translations[language];
-  const version = process.env.REACT_APP_VERSION || '1.0.0';
+  const version = packageJson.version;
 
   return (
     <footer className="footer">
